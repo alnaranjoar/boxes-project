9ea606d1242695aafbdb5f4709ec61b817d31428
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-08-04 18:25:33 -0500

    add first song, configure in javascript audio controls and progress bar according to the progress of the song

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 const rangeSlider = document.querySelector("#range-slider")
+const song=document.getElementById('audio')
+const playButton=document.getElementById('play-button')
+const pauseButton = document.getElementById('pause-button')
+const songStart=document.getElementById('song-start')
+const songDuration=document.getElementById('song-duration')
 
 rangeSlider.addEventListener('input',progressScript)
+rangeSlider.addEventListener('input',songSliderControl)
+playButton.addEventListener('click',playSong)
+pauseButton.addEventListener('click',pauseSong)
+
+function playSong() {
+    intervalo = setInterval(songTimeUpdater,50)
+    song.play()
+    playButton.classList.add('hide')
+    pauseButton.classList.remove('hide')
+}
+
+function songTimeUpdater() {
+    var currentTime = song.currentTime
+    
+    var minute = Math.floor((currentTime / 60) % 60)
+    var second = Math.floor(currentTime % 60)
+    second = (second < 10)? '0' + second : second
+
+    songStart.innerHTML = minute + ':' + second
+    rangeSlider.value = currentTime
+    progressScript()
+}
+
+function pauseSong() {
+    clearInterval(intervalo)
+    song.pause()
+    playButton.classList.remove('hide')
+    pauseButton.classList.add('hide')
+}
+
+
+function songTime() {
+    var songDurationSecs = song.duration
+    rangeSlider.max = songDurationSecs
+
+    var minute = Math.floor((songDurationSecs / 60) % 60)
+    var second = Math.floor(songDurationSecs % 60)
+    second = (second < 10)? '0' + second : second
+
+    songDuration.innerHTML = minute + ':' + second
+
+}
+
+songTime()
 
 function progressScript() {
     const sliderValue = rangeSlider.value;
-    rangeSlider.style.background = `linear-gradient(to right, #8643FF ${sliderValue}%, #D8D8D8 ${sliderValue}%)`
+    rangeSlider.style.background = `linear-gradient(to right, #8643FF ${(sliderValue/rangeSlider.max)*100}%, #D8D8D8 ${(sliderValue/rangeSlider.max)*100}%)`
 }
 
-progressScript()
+function songSliderControl() {
+    song.pause()
+    song.currentTime = rangeSlider.value
+}
+
