8378062910051e427b2652c8fa4ae4e02bc8c887
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-08-06 22:25:46 -0500

    created a playlist in index.js. added config in index.js to change songs. changes in html according to changes in js. little adjustments in css file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+//musicbox section
+let audio = document.querySelector('.audio')
+const cover = document.querySelector('.musicbox_img')
+const songName = document.querySelector('.musicbox_text--primary-bold')
+const artist = document.querySelector('.musicbox_text--secondary-regular')
 const rangeSlider = document.querySelector("#range-slider")
-const song=document.getElementById('audio')
-const playButton=document.getElementById('play-button')
+const playButton= document.getElementById('play-button')
 const pauseButton = document.getElementById('pause-button')
-const songStart=document.getElementById('song-start')
-const songDuration=document.getElementById('song-duration')
+const songStart= document.getElementById('song-start')
+const songDuration= document.getElementById('song-duration')
+const backBtn = document.getElementById('back-button')
+const forwardBtn = document.getElementById('forward-button')
+const nextBtn = document.getElementById('next-song')
+const previousBtn = document.getElementById('previous-song')
+let songIndex = 0
 
 rangeSlider.addEventListener('input',progressScript)
 rangeSlider.addEventListener('input',songSliderControl)
@@ -11,19 +20,62 @@ rangeSlider.addEventListener('mousedown',sliderSongPause)
 rangeSlider.addEventListener('mouseup',sliderSongPlay)
 playButton.addEventListener('click',playSong)
 pauseButton.addEventListener('click',pauseSong)
+backBtn.addEventListener('click', rewindSong)
+forwardBtn.addEventListener('click', forwardSong)
+previousBtn.addEventListener('click', previousSong)
+nextBtn.addEventListener('click',nextSong)
+
+songs = [
+    {
+        name: 'PHOTOGRAPH',
+        artist: 'Ed Sheeran',
+        img: 'https://i1.sndcdn.com/artworks-000300249612-tt0oxz-t500x500.jpg',
+        file: './assets/Photograph.mp3'
+    },
+    {
+        name: 'GRAMERCY PARK',
+        artist: 'Alicia Keys',
+        img: 'https://i1.sndcdn.com/artworks-yYK9dMwL4tIBz4Em-pQke7Q-t500x500.jpg',
+        file: './assets/Gramercy Park.mp3'
+    },
+    {
+        name: 'SOMEONE YOU LOVED',
+        artist: 'Lewis Capaldi',
+        img: 'https://i1.sndcdn.com/artworks-000553030245-5ydfwj-t500x500.jpg',
+        file: './assets/Someone You Loved.mp3'
+    },
+    {
+        name: 'ANGELS LIKE YOU',
+        artist: 'Miley Cyrus',
+        img: 'https://i1.sndcdn.com/artworks-NQS1EKtY1n1fDUVz-zzpYlQ-t500x500.jpg',
+        file: './assets/Angels Like You.mp3'
+    },
+]
+
+loadSongHtml()
+
+function loadSongHtml () {
+    cover.src = songs[songIndex].img
+    songName.innerHTML = songs[songIndex].name
+    artist.innerHTML = songs[songIndex].artist
+    audio.src = songs[songIndex].file
+    songStart.innerHTML = '0:00'
+    
+    setTimeout(songTime,200)
+}
 
 function playSong() {
     intervalo = setInterval(songTimeUpdater,50)
-    song.play()
+    audio.play()
     playButton.classList.add('hide')
     pauseButton.classList.remove('hide')
 }
 
 function songTimeUpdater() {
-    var currentTime = song.currentTime
+    let currentTime = audio.currentTime
     
-    var minute = Math.floor((currentTime / 60) % 60)
-    var second = Math.floor(currentTime % 60)
+    let minute = Math.floor((currentTime / 60) % 60)
+    let second = Math.floor(currentTime % 60)
     second = (second < 10)? '0' + second : second
 
     songStart.innerHTML = minute + ':' + second
@@ -33,25 +85,24 @@ function songTimeUpdater() {
 
 function pauseSong() {
     clearInterval(intervalo)
-    song.pause()
+    audio.pause()
     playButton.classList.remove('hide')
     pauseButton.classList.add('hide')
 }
 
-
 function songTime() {
-    var songDurationSecs = song.duration
+    
+    let songDurationSecs = audio.duration
+    console.log(songDurationSecs)
     rangeSlider.max = songDurationSecs
 
-    var minute = Math.floor((songDurationSecs / 60) % 60)
-    var second = Math.floor(songDurationSecs % 60)
+    let minute = Math.floor((songDurationSecs / 60) % 60)
+    let second = Math.floor(songDurationSecs % 60)
     second = (second < 10)? '0' + second : second
 
     songDuration.innerHTML = minute + ':' + second
 
 }
-
-songTime()
 
 function progressScript() {
     const sliderValue = rangeSlider.value;
@@ -59,14 +110,37 @@ function progressScript() {
 }
 
 function songSliderControl() {
-    song.currentTime = rangeSlider.value
+    audio.currentTime = rangeSlider.value
 }
 
 function sliderSongPause() {
-    console.log('pauseclick')
-    song.pause()
+    audio.pause()
 }
 
 function sliderSongPlay() {
-    song.play()
+    if(playButton.classList.contains('hide')) {
+        audio.play()
+    }
+}
+
+function rewindSong() {
+    audio.currentTime -= 10
+}
+
+function forwardSong() {
+    audio.currentTime += 10
+}
+
+function previousSong() {
+    audio.pause()
+    songIndex > 0 ? songIndex = songIndex - 1 : songIndex = 0
+    loadSongHtml()
+    audio.play()
+}
+
+function nextSong() {
+    audio.pause()
+    songIndex < songs.length - 1 ? songIndex = songIndex + 1 : songIndex = songs.length - 1
+    loadSongHtml()
+    audio.play()
 }
