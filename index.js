4d4d9eae35ba0d77b32509172b2a9b7bc3ba8cf9
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-08-08 18:35:46 -0500

    calendar complete with all features

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const previousMonthBtn = document.querySelector('.calendar_previous-month-btn')
 const calendarMonth = document.querySelector('.calendar_month')
 const calendarYear = document.querySelector('.calendar_year')
 const daysList = document.querySelector('#days-list')
+const calendarInput = document.querySelector('.calendar-input')
 
 let monthDates
 let currentDay
 let currentMonth
 let currentYear
+let prevMonthDays
+let nextMonthDays
 
 nextMonthBtn.addEventListener('click', changeToNextMonth)
 previousMonthBtn.addEventListener('click', changeToPreviousMonth)
@@ -31,6 +34,20 @@ function initializeCalendar() {
     })
 }
 
+function updateCalendarInput() {
+    let day = currentDay
+    let month = currentMonth
+
+    if (day < 10) {
+        day = '0' + day
+    } 
+
+    if (month < 10) {
+        month = '0' + month
+    } 
+    calendarInput.value = day + "/" + month + "/" + currentYear
+}
+
 function writeMonth() {
     
     let totalDays = getMonthDays(currentMonth)
@@ -50,31 +67,50 @@ function writeMonth() {
 
     daysSuma = start + totalDays
     missingNextMonthDays = (Math.ceil(daysSuma/7)*7)-daysSuma
-    console.log(missingNextMonthDays)
-    
 
     for (let i = 1; i <= missingNextMonthDays; i++) {
         daysList.innerHTML += `<li class="calendar_date next-month">${i}</li>`
     }
 
     monthDates = document.querySelectorAll('.calendar_date')
+    prevMonthDays = document.querySelectorAll('.previous-month')
+    nextMonthDays = document.querySelectorAll('.next-month')
 
     monthDates.forEach(date => {
-        date.addEventListener('click',changeColor)
+        date.addEventListener('click',setNewDay)
     })
+
+    prevMonthDays.forEach (day => {
+        day.addEventListener('click',changeToPreviousMonth)
+    })
+
+    nextMonthDays.forEach(day => {
+        day.addEventListener('click',changeToNextMonth)
+    })
+
+    changeColor()
+    
 }
 
-function changeColor(event) {
-    event.target.classList.toggle('painted')
-    let day = event.target.innerText
+function changeColor() {
+    
     monthDates.forEach(date => {
-        if(date.innerText != day) {
-            date.classList.remove('painted')
+        if((date.innerText == currentDay) && ((!date.classList.contains('previous-month')) && (!date.classList.contains('next-month')))) {
+            date.classList.add('painted')
         }
     })
+
+}
+
+function setNewDay(event) {
+    let day = event.target.innerText
+    currentDay = day
+
+    setNewDate()
 }
 
 function changeToPreviousMonth() {
+    
     if (currentMonth !== 1) {
         currentMonth --
     } else {
@@ -111,17 +147,13 @@ function startDay() {
     return (monthStartDay.getDay())
 }
 
-function lastMonth() {
-
-}
-
-function nextMonth(){
-
-}
-
 function setNewDate() {
+    
+    currentDate = new Date(currentYear,currentMonth-1,currentDay)
     calendarMonth.innerHTML = months[currentMonth-1]
     calendarYear.innerHTML = currentYear
+
+    updateCalendarInput()
 
     writeMonth()
 }
