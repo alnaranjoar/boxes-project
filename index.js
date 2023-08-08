ca0a0433b2b98b81e66af889aff2b62f6308921a
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-08-08 17:39:50 -0500

    added config to paint each month correctly, starting the right day and showing in gray previous and next month days

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const monthDates = document.querySelectorAll('.calendar_date')
 const months = ['January','Febrary','March','April','May','June','July','August','September','October','November','December']
 const nextMonthBtn = document.querySelector('.calendar_next-month-btn')
 const previousMonthBtn = document.querySelector('.calendar_previous-month-btn')
 const calendarMonth = document.querySelector('.calendar_month')
 const calendarYear = document.querySelector('.calendar_year')
+const daysList = document.querySelector('#days-list')
 
+let monthDates
 let currentDay
 let currentMonth
 let currentYear
-
-monthDates.forEach(date => {
-    date.addEventListener('click',changeColor)
-})
 
 nextMonthBtn.addEventListener('click', changeToNextMonth)
 previousMonthBtn.addEventListener('click', changeToPreviousMonth)
@@ -21,7 +18,9 @@ function initializeCalendar() {
     currentDay = currentDate.getDate()
     currentMonth = currentDate.getMonth() + 1
     currentYear = currentDate.getFullYear()
-
+    
+    writeMonth()
+   
     calendarMonth.innerHTML = months[currentMonth-1]
     calendarYear.innerHTML = currentYear
 
@@ -30,7 +29,39 @@ function initializeCalendar() {
             date.classList.add('painted')
         }
     })
-    //que cuando inicie el programa el calendario se cargue en el año, mes día en el que estamos.
+}
+
+function writeMonth() {
+    
+    let totalDays = getMonthDays(currentMonth)
+    let start = startDay()
+    let daysSuma
+    let missingNextMonthDays
+    
+    daysList.innerHTML=""
+
+    for (let i = start; i > 0; i--) {
+        daysList.innerHTML += `<li class="calendar_date previous-month">${getMonthDays(currentMonth-1)-(i-1)}</li>`
+    }
+    
+    for (let i = 1; i <= totalDays; i++) {
+        i === 1 ? daysList.innerHTML += `<li class="calendar_date first-day">${i}</li>` : daysList.innerHTML += `<li class="calendar_date">${i}</li>`
+    }
+
+    daysSuma = start + totalDays
+    missingNextMonthDays = (Math.ceil(daysSuma/7)*7)-daysSuma
+    console.log(missingNextMonthDays)
+    
+
+    for (let i = 1; i <= missingNextMonthDays; i++) {
+        daysList.innerHTML += `<li class="calendar_date next-month">${i}</li>`
+    }
+
+    monthDates = document.querySelectorAll('.calendar_date')
+
+    monthDates.forEach(date => {
+        date.addEventListener('click',changeColor)
+    })
 }
 
 function changeColor(event) {
@@ -44,15 +75,31 @@ function changeColor(event) {
 }
 
 function changeToPreviousMonth() {
-    // cambiar al mes anterior al presionar el botón
+    if (currentMonth !== 1) {
+        currentMonth --
+    } else {
+        currentMonth = 12
+        currentYear --
+    }
+
+    setNewDate()
 }
 
 function changeToNextMonth() {
-    // cambiar al mes siguiente al presionar el botón
+    
+    if (currentMonth !== 12) {
+        currentMonth ++
+    } else {
+        currentMonth = 1
+        currentYear ++
+    }
+
+    setNewDate()
 }
 
-function getMonthDays() {
-
+function getMonthDays(currentMonth) {
+    let totalDays = new Date (currentYear,currentMonth,0).getDate()
+    return totalDays
 }
 
 function isLeap() {
@@ -60,7 +107,7 @@ function isLeap() {
 }
 
 function startDay() {
-    let monthStartDay = new Date(currentYear,currentMonth,1)
+    let monthStartDay = new Date(currentYear,currentMonth-1,1)
     return (monthStartDay.getDay())
 }
 
@@ -73,7 +120,10 @@ function nextMonth(){
 }
 
 function setNewDate() {
-    
+    calendarMonth.innerHTML = months[currentMonth-1]
+    calendarYear.innerHTML = currentYear
+
+    writeMonth()
 }
 
 window.addEventListener('load', initializeCalendar)
